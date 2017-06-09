5e4afe714ff97ee4c6e2fa2a4ec4b8e34f06ff85
kevin <kevin@onfarminnovations.com>
2017-06-09 11:15:31 -0400

    changes for a more traditional eclipse project

---
+++ src/helloWorld.java
@@ -0,0 +1,11 @@
+
+public class helloWorld {
+
+    public static void main(String[] args) {
+        Amaze a = new Amaze();
+
+        // Prints "Hello World" into terminal window
+        System.out.println("Hello " + a.getText() + " World");
+
+    }
+}
